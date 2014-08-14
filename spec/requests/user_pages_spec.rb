758408cba926836c5c1fe30c9807646d5d22424f
Tim Hall <timothypatrickhall@gmail.com>
2014-08-13 18:31:43 -0700

    fixed conflict

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,8 +54,6 @@ describe "User pages" do
 				    it { should_not have_link('delete', href: user_path(admin)) }
 				end
 			end
-
-
 		end
 
 		describe "signup page" do
@@ -67,9 +65,18 @@ describe "User pages" do
 
 			describe "profile page" do 
 				let(:user) { FactoryGirl.create(:user) }
+				let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+				let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+				
 				before { visit user_path(user) }
 				it { should have_content(user.name) }
 				it { should have_title(user.name) }
+
+				describe "microposts" do
+					it  { should have_content(m1.content) }
+					it  { should have_content(m2.content) }
+					it  { should have_content(user.microposts.count) }
+				end
 			end
 
 			
@@ -162,5 +169,5 @@ describe "User pages" do
 	  			end
 
        end
-    end
+end
 #end
